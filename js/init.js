241ed731a6ea0c0fc6cfb308c58667d1863e009d
Sunpy <jonas_steinsvik@hotmail.no>
2018-01-05 16:26:01 +0100

    Load scripts

--- js/init.js
+++ js/init.js
@@ -1,6 +1,24 @@
 var file_uploader;
 
+function loadDependencies(){
+    let require = ["lzma.min.js", "replay-parser.js"];
+
+    let scripts = Array
+    .from(document.querySelectorAll('script'))
+    .map(scr => scr.src.split("/js/")[1]);
+
+    require.forEach(function(r){
+        if (!scripts.includes(r)){
+            var s = document.createElement("script");
+            s.src = "js/" + r;
+            document.head.append(s);
+        }
+    });
+}
+
 window.onload = function(){
+    loadDependencies();
+
     file_uploader = document.createElement("input");
     file_uploader.type = "file";
     file_uploader.name = "files[]";
