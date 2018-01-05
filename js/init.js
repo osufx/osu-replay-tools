72f53ef8a5e8ce84a7261cbbf6b46a7ac7ddff03
Sunpy <jonas_steinsvik@hotmail.no>
2018-01-05 15:41:34 +0100

    Handler for uploading replay file

--- js/init.js
+++ js/init.js
@@ -4,6 +4,18 @@ window.onload = function(){
     file_uploader = document.createElement("input");
     file_uploader.type = "file";
     file_uploader.name = "files[]";
+    file_uploader.addEventListener("change", uploadListener, false);
 
     document.body.appendChild(file_uploader);
 }
+
+var uploadListener = function(e) {
+	var file = e.target.files[0];
+	var reader = new FileReader();
+	reader.onload = (function(f) {
+		return function(progress) {
+            //Get data and run some sort of replay parser
+		};
+	})(file);
+	reader.readAsDataURL(file);
+};
